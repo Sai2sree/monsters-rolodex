320ee34faf2163bda00b276744df83a1fe9cb96f
Sree Sai Chaitanya Nagarajugari <chaitunagarajugari@gmail.com>
2020-04-25 21:49:56 -0700

    Updates

---
+++ precache-manifest.bf5bed4a605d8fab4a6616a38e0c20e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb15f43e88fddea5d6f61f5d2ed8f054",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a99594c40d0836656f38",
+    "url": "/static/css/main.08608f48.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99594c40d0836656f38",
+    "url": "/static/js/main.0a6db2fd.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
